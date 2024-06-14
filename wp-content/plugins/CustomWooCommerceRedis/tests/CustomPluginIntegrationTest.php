d36ca597580832f27eb6d32d64b2635dba20f2da
TooCodeSavvy <gisdon@protonmail.com>
2024-06-14 12:35:44 +0200

    UpdateCartItem-test is klaar. De functionaliteit zou goed moeten werken in productie, omdat de frontend de cart_item_key bijhoudt als een hidden input. Ik gebruik $cart_item_key = $woocommerce->cart->add_to_cart($product_id, $quantity); om de cart_item_key bij te houden die moet worden bijgewerkt, waardoor ik niet de cartKey zelf nodig heb om de unieke winkelwagen op te halen. Vervolgens wordt voor en na het updaten de winkelwagen gesynchroniseerd met Redis.

--- wp-content/plugins/CustomWooCommerceRedis/tests/CustomPluginIntegrationTest.php
+++ wp-content/plugins/CustomWooCommerceRedis/tests/CustomPluginIntegrationTest.php
@@ -49,7 +49,6 @@ class CustomPluginIntegrationTest extends TestCase {
         $this->customPlugin->syncCartToRedis();
 
         // Controleer of de winkelwageninformatie in Redis is opgeslagen
-        // Controleer of de winkelwageninformatie in Redis is opgeslagen
         $cartKey = $this->customPlugin->getCartKey();
         $cartData = $this->redisClient->get($cartKey);
 
@@ -58,7 +57,9 @@ class CustomPluginIntegrationTest extends TestCase {
         $cartData = unserialize($cartData);
 
         // Debug the cart data retrieved from Redis
-        var_dump($cartData); 
+        //var_dump($cartData); 
+        //var_dump($cartKey);
+
          // Validatie van winkelwagengegevens in Redis
         $this->assertNotEmpty($cartData, 'De winkelwageninformatie is niet in Redis opgeslagen');
         $this->assertEquals(32, $cartData[array_key_first($cartData)]['product_id']);
@@ -66,4 +67,69 @@ class CustomPluginIntegrationTest extends TestCase {
         $this->assertEquals(31, $cartData[array_keys($cartData)[1]]['product_id']);
         $this->assertEquals(2, $cartData[array_keys($cartData)[1]]['quantity']);
     }
+
+    public function testUpdateCartItem() {
+        global $woocommerce;
+
+        $woocommerce->cart->empty_cart();
+    
+        // Voeg een product toe aan de winkelwagen
+        $product_id = 32;
+        $quantity = 2;
+        $cart_item_key = $woocommerce->cart->add_to_cart($product_id, $quantity);
+
+       // var_dump($cart_item_key); 
+
+
+        // Synchroniseer winkelwagen met Redis
+        $this->customPlugin->syncCartToRedis();
+       
+
+        // Controleer of het item in de winkelwagen bestaat
+        if ($woocommerce->cart->get_cart_item($cart_item_key)) {
+            // Update de hoeveelheid
+            $new_quantity = 5;
+            $woocommerce->cart->set_quantity($cart_item_key, $new_quantity);
+          //  echo "De hoeveelheid is bijgewerkt naar $new_quantity<br>";
+        } else {
+            echo "Het winkelwagenitem kon niet worden gevonden.<br>";
+        }
+ 
+        // Controleer of het product is toegevoegd aan de winkelwagen
+        $cart_items = $woocommerce->cart->get_cart();
+ 
+        // Loop through each cart item
+       /* foreach ($cart_items as $cart_item_key => $cart_item) {
+            // Get the product object
+            $product = $cart_item['data'];
+
+            // Get product details
+            $product_name = $product->get_name();
+            $product_quantity = $cart_item['quantity'];
+            $product_price = $product->get_price();
+
+            // Display the product details
+            echo "Product Name: " . $product_name . "<br>";
+            echo "Quantity: " . $product_quantity . "<br>";
+            echo "Price: $" . $product_price . "<br>";
+            echo "-----------------------------------<br>";
+        } */
+    
+        // Synchroniseer winkelwagen met Redis
+        $this->customPlugin->syncCartToRedis();
+    
+        $cartKey = $this->customPlugin->getCartKey();
+        $cartData = $this->redisClient->get($cartKey);
+        $cartData = unserialize($cartData);
+        
+        //var_dump($cartData); 
+      //  var_dump($cartKey);
+      
+        // Controleer of de hoeveelheid correct is bijgewerkt
+        $this->assertEquals(5, $woocommerce->cart->get_cart_contents_count(), 'Aantal producten in de winkelwagen komt niet overeen na bijwerken van de hoeveelheid.');
+        $this->assertEquals(5, $cartData[array_key_first($cartData)]['quantity']);
+        $this->assertEquals($woocommerce->cart->get_cart_contents_count(), $cartData[array_key_first($cartData)]['quantity']);
+
+    }
+    
 }
