94a0e649d67e327b3161f91c4ad92902862e3678
TooCodeSavvy <gisdon@protonmail.com>
2024-06-30 20:38:46 +0200

    chore: Refactor create-wc-api-keys.php to improve logging and error handling

--- create-wc-api-keys.php
+++ create-wc-api-keys.php
@@ -2,7 +2,10 @@
 if (defined('WP_CLI') && WP_CLI) {
     class WC_API_Key_Command {
         function generate($args, $assoc_args) {
+            WP_CLI::log("Start van het genereren van API sleutel...");
+
             list($user_id, $description, $permissions) = $args;
+            WP_CLI::log("Gebruikers ID: $user_id, Beschrijving: $description, Permissies: $permissions");
 
             $data = [
                 'user_id' => $user_id,
@@ -19,10 +22,11 @@ if (defined('WP_CLI') && WP_CLI) {
                 WP_CLI::success($successMessage);
                 
                 $logFile = '/usr/share/nginx/html/output.log';
-                $message = "Your log message here.\n";
+                $message = "API sleutel succesvol aangemaakt voor gebruiker $user_id.\n";
                 if (file_put_contents($logFile, $message, FILE_APPEND | LOCK_EX) === false) {
-                    // Log error or take action if file writing fails
-                    error_log("Failed to write to {$logFile}");
+                    WP_CLI::log("Failed to write to {$logFile}");
+                } else {
+                    WP_CLI::log("Succesvol geschreven naar {$logFile}");
                 }
             }
         }
